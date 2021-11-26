8c547367b6a99bca2e26c89fea1bd41290e7e689
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-11-26 16:55:50 -0300

    arquivo controller: colocado return antes dos res.status para nao quebrar o codigo

--- controllers/Product.js
+++ controllers/Product.js
@@ -7,13 +7,13 @@ const create = async (req, res) => {
 
   if (message) return res.status(code).json({ err: { code: 'invalid_data', message } });
   
-  res.status(code).json(product);
+  return res.status(code).json(product);
 };
 
 const getAll = async (_req, res) => {
   const products = await Products.getAll();
 
-  res.status(200).json({ products });
+  return res.status(200).json({ products });
 };
 
 const findById = async (req, res) => {
@@ -22,7 +22,7 @@ const findById = async (req, res) => {
   const product = await Products.findById(id);
 
   if (product.err) return res.status(422).json(product);
-  res.status(200).json(product);
+  return res.status(200).json(product);
 };
 
 const update = async (req, res) => {
@@ -39,11 +39,13 @@ const deleteProduct = async (req, res) => {
   const { id } = req.params;
   const products = await Products.deleted(id);
   
-  if (!products) res.status(422).json({
+  if (!products) {
+ return res.status(422).json({
     err: { code: 'invalid_data', message: 'Wrong id format' },
-  });
+  }); 
+}
   
-  res.status(200).json({ message: 'produto deletado com sucesso' });
+  return res.status(200).json({ message: 'produto deletado com sucesso' });
 };
 
 module.exports = { 
